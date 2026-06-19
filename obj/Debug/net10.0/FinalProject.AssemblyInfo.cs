739259b59d4c68949f84e5d233501328ba000222
paulalas <paulirvin.alas@raybiztech.com>
2026-06-19 12:43:17 +0800

    added footers

--- obj/Debug/net10.0/FinalProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/FinalProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc24246c2bf5f0671d2ee7c2cd6de0c61d75404e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf12ebc66dcfb634844b5e166758c115cd16e56c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
