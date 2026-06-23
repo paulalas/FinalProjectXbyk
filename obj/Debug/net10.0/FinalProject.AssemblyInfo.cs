449aefc53662fdb4c0723563f6c38d829bc4eb56
paulalas <paulirvin.alas@raybiztech.com>
2026-06-23 14:03:16 +0800

    update herobanner

--- obj/Debug/net10.0/FinalProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/FinalProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+739259b59d4c68949f84e5d233501328ba000222")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ec59e15a25dd14e45ac05ff6f884658c4cd1257")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
